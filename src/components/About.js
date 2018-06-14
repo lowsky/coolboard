d20723aca8a35305ebb8a8098991bff18b3718dd
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2018-06-15 01:18:28 +0200

    cosmetic: About page

--- src/components/About.js
+++ src/components/About.js
@@ -55,7 +55,9 @@ export default class About extends Component {
             <h2>How does it work?</h2>
             <p>
               You will need to Sign-up via email and{' '}
-              <Link to="/login">Log-in</Link>.
+              <Link to="/login">
+                <Icon size="big" name="sign in"/>authenticate here
+              </Link>
             </p>
             <p>
               Then you can create and share a board
@@ -70,6 +72,36 @@ export default class About extends Component {
               React frontend with Apollo served by
               netlify.com
             </p>
+          </Segment>
+          <Segment basic>
+            <h2>How to sign-in?</h2>
+            <div
+              style={{
+                border: 'solid 1px grey',
+                background: '#ddd',
+                textAlign: 'left',
+                padding: '8px',
+              }}>
+              <p>
+                We are using the service of
+                <a href="https://auth0.com/">Auth0</a>
+                to allow signing-in via OAuth using
+                Google/Twitter or email/password
+                automatically.
+              </p>
+              <p>
+                <b>About Privacy:</b>
+                <br />We will use your account
+                information for authenticating and
+                storing your boards under your account.
+                <br />
+                We might rarely send any emails for
+                notifying about changes related to
+                providing this service (e.g. news, any
+                case of outage or maintenance time) or
+                updated to this.
+              </p>
+            </div>
           </Segment>
           <Segment basic>
             <h2>Credits / Copyright</h2>
