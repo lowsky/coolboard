14ced09b7cfe7aa94dc5e1c746da88ae4c2d3a55
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2018-07-09 23:07:05 +0200

    fix not using service worker by un-registering any old one.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import unregisterServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
-// Instead of integrating the
-// css here, with running webpack bundling every time while developing,
-// I put this into the index page:
-// <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-//
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-unregisterServiceWorker();
+unregister();
