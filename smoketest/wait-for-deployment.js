68ff33eef5a6f3962b9d73c0846750c536f84366
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2019-07-14 13:56:28 +0200

    reduce delay and fix messages in wait-for-delay before smoketest

--- smoketest/wait-for-deployment.js
+++ smoketest/wait-for-deployment.js
@@ -1,24 +1,26 @@
 // eslint-disable-next-line no-unused-vars,no-undef
 var rp = require('request-promise');
 
-const url = (process.env.CYPRESS_baseUrl || 'http://localhost:3000') + '/meta.json';
+const url =
+  (process.env.CYPRESS_baseUrl ||
+    'http://localhost:3000') + '/meta.json';
 
 const EXPECTED_GIT_SHA1 = process.env.GIT_SHA1 || '';
 
 // fast-fail
 if (!EXPECTED_GIT_SHA1) {
   console.error(
-    'git commit check failed, missing ENV variable: GIT_SHA1 !'
+    'git commit check failed: missing ENV variable: GIT_SHA1 !'
   );
   process.exit(1);
 }
 
-console.error(
+console.log(
   'Checking if deployed site is based on this GIT_SHA1:',
   EXPECTED_GIT_SHA1
 );
 
-let intTimeMs = 10000; // interval duration
+let intTimeMs = 60000; // interval duration
 let ntimes = 6; // number retries
 
 var options = {
@@ -39,9 +41,11 @@ function checkSiteVersion() {
         console.log(
           `Deployed site is based on the expected commit: ${response.GIT_SHA1} ✓`
         );
+        clearInterval(interval);
+        process.exit(0);
       }
       console.warn(
-        `Deployed site is based on this commit: ${response.GIT_SHA1} - expected is ${EXPECTED_GIT_SHA1}`
+        `Deployed site is based on this commit: ${response.GIT_SHA1} - expected was ${EXPECTED_GIT_SHA1}`
       );
     })
     .catch(err => {
@@ -53,22 +57,24 @@ function checkSiteVersion() {
 }
 
 const retryCheck = () => {
-  console.log(
-    ' Check triggered! still %d times',
-    ntimes
-  );
-
   checkSiteVersion();
 
   ntimes--;
 
   if (!ntimes || validGitCommitReceived) {
-    console.log('cleared!');
     clearInterval(interval);
     if (validGitCommitReceived) {
       process.exit(0);
-    } else process.exit(-1);
+    }
+    console.error('Timeout. Error.');
+    process.exit(-1);
   }
+  console.log(
+    ' Will retry check another %d times',
+    ntimes
+  );
 };
+
+checkSiteVersion();
 
 const interval = setInterval(retryCheck, intTimeMs);
