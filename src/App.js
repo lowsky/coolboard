a2fdbc92f5cca14b72f3276c2695335045ef9b10
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2019-08-29 12:13:54 +0200

    extract setting up apollo client to own module

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import React, { lazy, Suspense } from 'react';
 
 import { Loader } from 'semantic-ui-react';
 
-import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-/* http link */
-import { createHttpLink } from 'apollo-link-http';
-import { ApolloLink } from 'apollo-link';
 
-import { createNetworkStatusNotifier } from 'react-apollo-network-status';
-
+import { setupGraphQLClient } from './setupGraphQLClient';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-
 import {
-  Switch,
-  Route,
   BrowserRouter,
   Link,
+  Route,
+  Switch,
 } from 'react-router-dom';
 
 import Auth from './authentication/auth';
@@ -29,6 +22,7 @@ import Home from './pages/home/Home';
 import { FullVerticalContainer } from './common/FullVerticalContainer';
 import { ProfileHeader } from './common/ProfileHeader';
 import { GeneralErrorHandler } from './common/GeneralErrorHandler';
+
 const CoolBoard = lazy(() =>
   import('./components/CoolBoard').then(module => ({
     default: module.CoolBoard,
@@ -40,52 +34,10 @@ const Boards = lazy(() =>
   }))
 );
 
-const node_env = process.env.NODE_ENV;
-const SRV_HOST_PORT_DOMAIN =
-  process.env.REACT_APP_SERVER_HOST;
-
-const IS_LOCALHOST =
-  !SRV_HOST_PORT_DOMAIN ||
-  SRV_HOST_PORT_DOMAIN.indexOf('localhost') >= 0;
-
-const IS_PROD = node_env === 'production';
-
-const secureConnection = IS_PROD || !IS_LOCALHOST;
-
-const uri =
-  (secureConnection ? 'https://' : 'http://') +
-  SRV_HOST_PORT_DOMAIN;
-
-// Create a Http link
-let httpLink = createHttpLink({
-  uri:
-    IS_LOCALHOST || IS_PROD
-      ? '/.netlify/functions/graphql'
-      : uri,
-});
-
-const middlewareAuthLink = new ApolloLink(
-  (operation, forward) => {
-    const token = localStorage.getItem('access_token');
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-    return forward(operation);
-  }
-);
-
 const {
+  client,
   NetworkStatusNotifier,
-  link: networkStatusNotifierLink,
-} = createNetworkStatusNotifier();
-
-const client = new ApolloClient({
-  link: networkStatusNotifierLink.concat(middlewareAuthLink.concat(httpLink)),
-  cache: new InMemoryCache(),
-});
+} = setupGraphQLClient();
 
 const auth = new Auth(client);
 
---
+++ src/setupGraphQLClient.js
@@ -0,0 +1,63 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createNetworkStatusNotifier } from 'react-apollo-network-status';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+
+export const setupGraphQLClient = () => {
+  // eslint-disable-next-line no-undef
+  const node_env = process.env.NODE_ENV;
+  const SRV_HOST_PORT_DOMAIN =
+    // eslint-disable-next-line no-undef
+    process.env.REACT_APP_SERVER_HOST;
+
+  const IS_LOCALHOST =
+    !SRV_HOST_PORT_DOMAIN ||
+    SRV_HOST_PORT_DOMAIN.indexOf('localhost') >= 0;
+
+  const IS_PROD = node_env === 'production';
+
+  const secureConnection = IS_PROD || !IS_LOCALHOST;
+
+  const uri =
+    (secureConnection ? 'https://' : 'http://') +
+    SRV_HOST_PORT_DOMAIN;
+  // Create a Http link
+  let httpLink = createHttpLink({
+    uri:
+      IS_LOCALHOST || IS_PROD
+        ? '/.netlify/functions/graphql'
+        : uri,
+  });
+
+  const middlewareAuthLink = new ApolloLink(
+    (operation, forward) => {
+      const token = localStorage.getItem(
+        'access_token'
+      );
+
+      operation.setContext({
+        headers: {
+          authorization: token
+            ? `Bearer ${token}`
+            : '',
+        },
+      });
+      return forward(operation);
+    }
+  );
+
+  const {
+    NetworkStatusNotifier,
+    link: networkStatusNotifierLink,
+  } = createNetworkStatusNotifier();
+
+  const client = new ApolloClient({
+    link: networkStatusNotifierLink.concat(
+      middlewareAuthLink.concat(httpLink)
+    ),
+    cache: new InMemoryCache(),
+  });
+
+  return { client, NetworkStatusNotifier };
+};
