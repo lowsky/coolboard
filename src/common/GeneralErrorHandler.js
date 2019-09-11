7133b20778fa26d3464eb305c7bf0b9e18ec15da
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2019-09-11 22:02:26 +0200

    improve authentificationi by allowing user/passwd auto insertion, button for re-login / refresh auth token

--- src/common/GeneralErrorHandler.js
+++ src/common/GeneralErrorHandler.js
@@ -2,26 +2,40 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { Icon, Message } from 'semantic-ui-react';
+import { Button, Icon, Message } from 'semantic-ui-react';
 
 // Error name, used on the server side, too
 const NotAuthorizedError = 'NotAuthorizedError';
 
 export const GeneralErrorHandler = ({
   NetworkStatusNotifier,
+  auth,
 }) => (
   <NetworkStatusNotifier
     render={({ error }) => {
       if (error) {
         const { graphQLErrors, networkError } = error;
+
+        const Relogin = () => (!auth.isAuthenticated()) &&<div>
+          <span>Not authenticated.</span>
+          <Button onClick={() => {
+            auth.refresh()
+          }}>
+            Please re-login / refresh auth
+          </Button>
+        </div>
+
         if (graphQLErrors) {
           const notAuthErr = graphQLErrors.find(
             err => err.name === NotAuthorizedError
           );
 
           if (notAuthErr) {
+            debugger
+
             return (
               <Message error>
+                <Relogin />
                 <strong>
                   You will need to be authenticated
                   to see or create Boards or change
@@ -36,19 +50,24 @@ export const GeneralErrorHandler = ({
               </Message>
             );
           }
+
           return (
             <Message error>
-              {graphQLErrors
+              <Relogin />
+              {
+                graphQLErrors
                 .filter(error => error.message)
                 .map(error => error.message)
                 .map((message, idx) => (
                   <p key={idx}>{message}</p>
-                ))}
+                ))
+              }
             </Message>
           );
         } else if (networkError) {
           return (
             <Message error>
+              <Relogin />
               <p>
                 <strong>Network Error:</strong>{' '}
                 {networkError.message}
@@ -60,6 +79,7 @@ export const GeneralErrorHandler = ({
         console.log('unknown error!', error);
         return (
           <Message error>
+            <Relogin />
             <strong>Unknown error!</strong>
             <p>
               You could find more details in the
