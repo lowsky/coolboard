bce8d8c7cdd7769c4466de2ec19ec14bb8f82a3b
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2019-09-28 21:34:26 +0200

    remove obsolete test file

--- src/asyncSetup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { setupGraphQLClient } from './setupGraphQLClient';
-
-setupGraphQLClient();
-
-// eslint-disable-next-line react/display-name
-export default () => <h1>Lazy</h1>;
