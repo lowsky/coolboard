51072c435fc4b933cbe13a48e07b54a098362cff
lowsky <217931+lowsky@users.noreply.github.com>
2020-11-01 19:13:42 +0100

    fix trigger , add diagnosis

--- trigger-circleci-smoke.sh
+++ trigger-circleci-smoke.sh
@@ -9,21 +9,24 @@ CIRCLE_API_USER_TOKEN=1851154c07ebcb2a01c99a5851a79fe9e8ca893c
 #DEPLOYED_SHA1=811fbecf6b228aa46a9cb72224cd8e5d1e467ff5
 #DEPLOYED_SHA1=b0d003070bbbd81bf1e8b15f3cd76cd389b13d72
 DEPLOYED_SHA1=$VERCEL_GITHUB_COMMIT_SHA
-DEPLOYED_SHA1=
 
 #BRANCH=activate-vercel-serverless
 BRANCH=$VERCEL_GITHUB_COMMIT_REF
-CIRCLE_PR_NUMBER=565
+# obsolete - CIRCLE_PR_NUMBER=565
 
-SUT_URL=$VERCEL_GITHUB_DEPLOYMENT
 SUT_URL=https://hands-on-application-building-with-graph-ql-and-reac-790uxmthz.vercel.app/
+SUT_URL=$VERCEL_GITHUB_DEPLOYMENT
+echo testing "$SUT_URL"
 
 CIRCLE="https://circleci.com/api/v1.1/project"
 PROJ="$CIRCLE/github/lowsky/Hands-on-Application-Building-with-GraphQL-and-React/tree/${BRANCH}"
 
-URL="$PROJ?build_parameters%5BCIRCLE_JOB%5D=build&build_parameters%5BDEPLOYED_SHA1%5D=$DEPLOYED_SHA1&build_parameters%5BCIRCLE_PR_NUMBER%5D=$CIRCLE_PR_NUMBER&build_parameters%5BTEST_URL%5D=http://sut"
+#URL="$PROJ?build_parameters%5BCIRCLE_JOB%5D=build&build_parameters%5BDEPLOYED_SHA1%5D=$DEPLOYED_SHA1&build_parameters%5BCIRCLE_PR_NUMBER%5D=$CIRCLE_PR_NUMBER&build_parameters%5BTEST_URL%5D=$SUT_URL"
+URL="$PROJ?build_parameters%5BCIRCLE_JOB%5D=build&build_parameters%5BDEPLOYED_SHA1%5D=$DEPLOYED_SHA1&build_parameters%5BTEST_URL%5D=$SUT_URL"
+
+echo triggering url : $URL
 
 curl -u ${CIRCLE_API_USER_TOKEN}: $URL \
-    -d "build_parameters[CIRCLE_PR_NUMBER]=${CIRCLE_PR_NUMBER}" \
     -d "build_parameters[DEPLOYED_SHA1]=$DEPLOYED_SHA1" \
-    -d "build_parameters[CYPRESS_baseUrl]=$SUT_URL"
+    -d "build_parameters[CYPRESS_baseUrl]=$SUT_URL" \
+#   -d "build_parameters[CIRCLE_PR_NUMBER]=${CIRCLE_PR_NUMBER}" \
