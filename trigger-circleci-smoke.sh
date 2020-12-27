3373892dac9c1ba0867bf4da93866e95667ebb3d
lowsky <217931+lowsky@users.noreply.github.com>
2020-12-27 17:02:00 +0100

    adapt, upgrade circle.ci pipeline definition (#612)
    * add ci parameter gh-pr , test-url
    
    * add workflow to circleci pipeline
    
    * migrate to circleci api v2
    
    * run cypress as specified in smoketest's package.json instead of docker version

--- trigger-circleci-smoke.sh
+++ trigger-circleci-smoke.sh
@@ -10,29 +10,33 @@ echo VERCEL_URL:  $VERCEL_URL
 # token: "trigger-from-netlify-hook"
 CIRCLE_API_USER_TOKEN=1851154c07ebcb2a01c99a5851a79fe9e8ca893c
 
-#DEPLOYED_SHA1=811fbecf6b228aa46a9cb72224cd8e5d1e467ff5
-#DEPLOYED_SHA1=b0d003070bbbd81bf1e8b15f3cd76cd389b13d72
-DEPLOYED_SHA1=$VERCEL_GITHUB_COMMIT_SHA
+#COMMIT
+#VERCEL_GITHUB_COMMIT_SHA=b0d003070bbbd81bf1e8b15f3cd76cd389b13d72
 
 #BRANCH=activate-vercel-serverless
 BRANCH=$VERCEL_GITHUB_COMMIT_REF
 
 #SUT_URL=https://hands-on-application-building-with-graph-ql-and-reac-790uxmthz.vercel.app/
-SUT_URL=https://$VERCEL_URL
+TEST_URL=https://$VERCEL_URL
 echo
-echo testing "$SUT_URL"
+echo testing "$TEST_URL"
 echo
 
-CIRCLE="https://circleci.com/api/v1.1/project"
-PROJ="$CIRCLE/github/lowsky/Hands-on-Application-Building-with-GraphQL-and-React/tree/${BRANCH}"
-
-URL="$PROJ?build_parameters%5BCIRCLE_JOB%5D=build"
-# not needed any longer? &build_parameters%5BDEPLOYED_SHA1%5D=$DEPLOYED_SHA1&build_parameters%5BCYPRESS_baseUrl%5D=$SUT_URL"
+CIRCLE="https://circleci.com/api/v2/project"
+URL="$CIRCLE/gh/lowsky/Hands-on-Application-Building-with-GraphQL-and-React/pipeline"
 
 echo
 echo triggering url : $URL
 echo
 
-curl -u ${CIRCLE_API_USER_TOKEN}: $URL \
-    -d "build_parameters[DEPLOYED_SHA1]=$DEPLOYED_SHA1" \
-    -d "build_parameters[CYPRESS_baseUrl]=$SUT_URL"
+curl --request POST \
+  -u ${CIRCLE_API_USER_TOKEN}: $URL \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"branch": "'$BRANCH'",
+	"parameters":
+    {
+			"deployed-sha": "'$VERCEL_GITHUB_COMMIT_SHA'",
+			"test-url": "'$TEST_URL'"
+		}
+}'
