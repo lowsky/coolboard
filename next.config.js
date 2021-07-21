624a96a499b946088eb32a881b4d7d5247e0a556
lowsky <217931+lowsky@users.noreply.github.com>
2021-07-21 21:22:51 +0200

    upgrade yarn.lock deps, switch to webpack4

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   experimental: {
     craCompat: true,
   },
+  webpack5: false,
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
