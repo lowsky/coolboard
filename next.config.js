591a8b95edac808a83340990f92ff822cf86834f
lowsky <217931+lowsky@users.noreply.github.com>
2021-07-23 22:29:43 +0200

    enable webpack5, no special next images settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   target: "serverless",
-  webpack5: false,
+  // true by default:
+  // webpack5: false,
+  //
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
-  images: {
-    disableStaticImages: true
-  }
+  // images: { disableStaticImages: true }
 }
