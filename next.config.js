c33e26d11e8217391afa8438519dcdbe1849ca3b
lowsky <217931+lowsky@users.noreply.github.com>
2021-07-27 00:58:05 +0200

    adjust next.conf for webpack5

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
-const withImages = require('next-images')
-
 module.exports = {
-  ...withImages(),
+  // prisma binary was not available when webpack was bundling it, so netlify's zip-it... did not find the
+  // binaries, see https://github.com/prisma/prisma/issues/6051#issuecomment-831136748
+  // netlify is working on it...
+  webpack: (config, { isServer } ) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+      // When working on webpack5 there was this runtime-error:
+      // Module "ts-tiny-invariant" was not found. PR...
+      config.externals.push('ts-is-defined')
+      // config.externals.push('ts-tiny-invariant')
+    }
+
+    return config
+  },
   // Target must be serverless (for use with netlify)
   target: "serverless",
-  // would be true by default. Need to switch back for prisma
-  // to avoid this error:
-  // Module not found: Error: Can't resolve '_http_common' in '/opt/build/repo/node_modules/@prisma/client/runtime'
-  webpack5: false,
   //
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
