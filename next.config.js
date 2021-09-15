fe87e6668bfd54a7ca70d0dd31d33f422ce24506
lowsky <217931+lowsky@users.noreply.github.com>
2021-09-15 08:46:52 +0200

    improve netlify build

--- next.config.js
+++ next.config.js
@@ -19,4 +19,12 @@ module.exports = {
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   // images: { disableStaticImages: true }
+
+  // for builds on netlify, it recommends this setting:
+  // "For faster deploy times, build IDs should be set to a static value.
+  // To do this, set generateBuildId: () => 'build' in your next.config.js"
+  // https://nextjs.org/docs/api-reference/next.config.js/configuring-the-build-id
+  generateBuildId: () => 'build',
+
+  poweredByHeader: false,
 }
