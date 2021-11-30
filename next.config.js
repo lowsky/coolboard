1cb523a477ce135d6c31ed6cfd20887a08ceff01
lowsky <217931+lowsky@users.noreply.github.com>
2021-11-30 15:58:31 +0100

    disable eslint while building

--- next.config.js
+++ next.config.js
@@ -13,6 +13,13 @@ module.exports = {
 
     return config
   },
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   // Target must be serverless (for use with netlify)
   target: "serverless",
   //
