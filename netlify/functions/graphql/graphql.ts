07639da890a1e6a926fec9706956407628abf3f9
lowsky <217931+lowsky@users.noreply.github.com>
2021-11-30 15:40:18 +0100

    adapt for work on netlify

---
+++ netlify/functions/graphql/graphql.ts
@@ -0,0 +1,56 @@
+import './open-telemetry'
+import { ApolloServer } from 'apollo-server-lambda';
+
+import resolvers from '../../../server/src/resolvers/resolvers';
+import { typeDefs } from '../../../server/src/schema/apiSchema';
+
+import { isLocalDev } from '../../../server/src/helpers/logging';
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient({
+  log: isLocalDev ? ['query', 'info', `warn`, `error`]:
+    ['info', 'warn', 'error']
+});
+
+
+let handler = (event, context, callback) => {
+
+  let lambdaServer;
+  lambdaServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+    debug: isLocalDev,
+    introspection: isLocalDev,
+
+    context: ({event}) => ({
+      event: {
+        headers: event.headers
+      },
+      prisma,
+    })
+  });
+
+  const handler = lambdaServer.createHandler();
+
+  try {
+    if(isLocalDev) {
+      return handler({
+        ...event,
+        // Workaround for apollo-lambda crashes when running with netlify dev
+        // "Unable to determine event source based on event." (in @vendia/serverless-express)
+        requestContext: {}
+      }, context, callback);
+    }
+    return handler(event, context, callback);
+  }
+  catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "GraphQL server: sorry, an error: " + e }),
+    };
+  }
+}
+
+export { handler };
