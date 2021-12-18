ceeb9ed2e520310260e07f369e0e1ad3fcfbc116
lowsky <217931+lowsky@users.noreply.github.com>
2021-12-18 22:47:59 +0100

    more formatting

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ module.exports = {
   // prisma binary was not available when webpack was bundling it, so netlify's zip-it... did not find the
   // binaries, see https://github.com/prisma/prisma/issues/6051#issuecomment-831136748
   // netlify is working on it...
-  webpack: (config, { isServer } ) => {
+  webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals.push('@prisma/client')
+      config.externals.push('@prisma/client');
       // When working on webpack5 there was this runtime-error:
       // Module "ts-tiny-invariant" was not found. PR...
-      config.externals.push('ts-is-defined')
+      config.externals.push('ts-is-defined');
       // config.externals.push('ts-tiny-invariant')
     }
 
@@ -22,4 +22,4 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+};
