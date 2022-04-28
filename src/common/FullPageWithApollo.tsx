5161cc6d2ae29428f848f2296976622b37bd0396
lowsky <217931+lowsky@users.noreply.github.com>
2022-04-28 23:44:01 +0200

    reload queries after login

--- src/common/FullPageWithApollo.tsx
+++ src/common/FullPageWithApollo.tsx
@@ -3,11 +3,13 @@ import { ApolloProvider } from '@apollo/client';
 
 import { FullVerticalContainer } from './FullVerticalContainer';
 import { setupGraphQLClient } from '../setupGraphQLClient';
+import { ReloadAfterLogin } from '../auth/ReloadAfterLogin';
 
 const client = setupGraphQLClient();
 
 const FullPageWithApollo = ({ children }) => (
   <FullVerticalContainer data-cy="full-container">
+    <ReloadAfterLogin client={client} />
     <ApolloProvider client={client}>{children}</ApolloProvider>
   </FullVerticalContainer>
 );
