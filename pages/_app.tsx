c08796283e8ab90e30149d16f359d82bbd1ee9d8
lowsky <217931+lowsky@users.noreply.github.com>
2022-05-03 22:51:23 +0200

    start to migrate to chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,54 @@
-import Head from 'next/head';
-import type { AppProps } from 'next/app';
-import { ClerkProvider } from '@clerk/nextjs';
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-import '../public/index.css';
-import '../src/fomantic/dist/semantic.min.css';
+import "../public/index.css";
+import "../src/fomantic/dist/semantic.min.css";
 
-import { InstanaEumScripts } from '../src/common/instanaEumScripts';
-import { Footer } from '../src/components/Footer';
+import { InstanaEumScripts } from "../src/common/instanaEumScripts";
+import { Footer } from "../src/components/Footer";
 
 // Head's title and view-port-meta needs to go here, see https://nextjs.org/docs/messages/no-document-viewport-meta
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
+  /*
+More background about theming:
+https://chakra-ui.com/docs/styled-system/theming/theme
+https://chakra-ui.com/docs/styled-system/theming/customize-theme#customizing-component-styles
+ */
+
+  const theme = extendTheme({
+    config: {
+      initialColorMode: 'light',
+    }, components: {
+      Text: {
+        baseStyle: {
+          mb: '1rem'
+        }
+      Heading: {
+        baseStyle: {
+          fontWeight: '700',
+          mb: '1rem'
+        },
+      },
+    },
+    styles: {
+      global: (props) => ({
+        body: {
+          fontFamily: 'Inter',
+        },
+        a: {
+          color: mode("#3273dc", "blue.200")(props), _hover: {
+            textDecoration: "underline"
+          }
+        }
+      })
+    }
+  });
+
+  return (<>
       <Head>
         <title>Coolboard - Hands-on Application Building with GraphQL</title>
         <meta
@@ -23,15 +59,15 @@ export default function App({ Component, pageProps }: AppProps) {
       {false && <InstanaEumScripts />}
       <div
         style={{
-          overflow: 'auto',
-          flex: 1,
+          overflow: "auto", flex: 1
         }}>
-        <ClerkProvider {...pageProps}>
-          <Component {...pageProps} />
-        </ClerkProvider>
+        <ChakraProvider theme={theme}>
+          <ClerkProvider {...pageProps}>
+            <Component {...pageProps} />
+          </ClerkProvider>
+        </ChakraProvider>
       </div>
 
       <Footer />
-    </>
-  );
+    </>);
 }
