c6a0dd2a5451d166e58789b86bcaeb68d0e1d802
lowsky <217931+lowsky@users.noreply.github.com>
2022-05-03 23:31:53 +0200

    minor formatting, cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
-import Head from "next/head";
-import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
-import "../public/index.css";
-import "../src/fomantic/dist/semantic.min.css";
+import '../public/index.css';
 
-import { InstanaEumScripts } from "../src/common/instanaEumScripts";
-import { Footer } from "../src/components/Footer";
+import { InstanaEumScripts } from '../src/common/instanaEumScripts';
+import { Footer } from '../src/components/Footer';
 
 // Head's title and view-port-meta needs to go here, see https://nextjs.org/docs/messages/no-document-viewport-meta
 
@@ -22,15 +21,17 @@ https://chakra-ui.com/docs/styled-system/theming/customize-theme#customizing-com
   const theme = extendTheme({
     config: {
       initialColorMode: 'light',
-    }, components: {
+    },
+    components: {
       Text: {
         baseStyle: {
-          mb: '1rem'
-        }
+          mb: '1rem',
+        },
+      },
       Heading: {
         baseStyle: {
           fontWeight: '700',
-          mb: '1rem'
+          mb: '1rem',
         },
       },
     },
@@ -40,15 +41,17 @@ https://chakra-ui.com/docs/styled-system/theming/customize-theme#customizing-com
           fontFamily: 'Inter',
         },
         a: {
-          color: mode("#3273dc", "blue.200")(props), _hover: {
-            textDecoration: "underline"
-          }
-        }
-      })
-    }
+          color: mode('#3273dc', 'blue.200')(props),
+          _hover: {
+            textDecoration: 'underline',
+          },
+        },
+      }),
+    },
   });
 
-  return (<>
+  return (
+    <>
       <Head>
         <title>Coolboard - Hands-on Application Building with GraphQL</title>
         <meta
@@ -59,7 +62,8 @@ https://chakra-ui.com/docs/styled-system/theming/customize-theme#customizing-com
       {false && <InstanaEumScripts />}
       <div
         style={{
-          overflow: "auto", flex: 1
+          overflow: 'auto',
+          flex: 1,
         }}>
         <ChakraProvider theme={theme}>
           <ClerkProvider {...pageProps}>
@@ -69,5 +73,6 @@ https://chakra-ui.com/docs/styled-system/theming/customize-theme#customizing-com
       </div>
 
       <Footer />
-    </>);
+    </>
+  );
 }
