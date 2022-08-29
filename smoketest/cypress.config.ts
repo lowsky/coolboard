eda9b2805938215823e58d8cf491345c6da7dff6
lowsky <217931+lowsky@users.noreply.github.com>
2022-08-29 09:25:35 +0200

    cypress fix for main branch

--- smoketest/cypress.config.ts
+++ smoketest/cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://www.coolboard.fun',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 
