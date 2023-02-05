5a8f73afa23818062b9c5a9c4f58efd56cf42218
lowsky <217931+lowsky@users.noreply.github.com>
2023-02-05 15:50:34 +0100

    remove workaround for netlify, adding @prisma/client as 'externals' in nextjs' webpack - config (#2083)

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
-module.exports = {
-  // prisma binary was not available when webpack was bundling it, so netlify's zip-it... did not find the
-  // binaries, see https://github.com/prisma/prisma/issues/6051#issuecomment-831136748
-  // netlify is working on it...
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-      // When working on webpack5 there was this runtime-error:
-      // Module "ts-tiny-invariant" was not found. PR...
-      config.externals.push('ts-is-defined');
-      // config.externals.push('ts-tiny-invariant')
-    }
+// @ts-check
 
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader',
-    });
-
-    return config;
-  },
-
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   reactStrictMode: true,
-
-  // "For faster deploy times, build IDs should be set to a static value..."
-  generateBuildId: () => 'build',
 
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -31,3 +12,5 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
 };
+
+module.exports = nextConfig;
