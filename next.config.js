3fc1c12454589cf5806c96467429049b20735664
lowsky <217931+lowsky@users.noreply.github.com>
2023-02-05 14:41:23 +0100

    remove adding 'prismaclient' as 'externals' in nextjs' webpack - config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  // prisma binary was not available when webpack was bundling it, so netlify's zip-it... did not find the
-  // binaries, see https://github.com/prisma/prisma/issues/6051#issuecomment-831136748
-  // netlify is working on it...
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-      // When working on webpack5 there was this runtime-error:
-      // Module "ts-tiny-invariant" was not found. PR...
-      config.externals.push('ts-is-defined');
-      // config.externals.push('ts-tiny-invariant')
-    }
-
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
