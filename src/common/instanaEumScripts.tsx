4c37746fd486b84ddb49734e6fee03b685920c4e
lowsky <217931+lowsky@users.noreply.github.com>
2023-07-19 14:17:56 +0200

    add config for instana eum reporting URL

--- src/common/instanaEumScripts.tsx
+++ src/common/instanaEumScripts.tsx
@@ -1,6 +1,7 @@
 import Script from 'next/script';
 
 const NEXT_PUBLIC_INSTANA_EUM_KEY = process.env.NEXT_PUBLIC_INSTANA_EUM_KEY;
+const NEXT_PUBLIC_REPORTING_URL = process.env.NEXT_PUBLIC_REPORTING_URL;
 
 export const InstanaEumScripts = () => (
   <>
@@ -11,7 +12,7 @@ export const InstanaEumScripts = () => (
           },
           n.q = [], n.v = 2, n.l = 1 * new Date)})(window,'InstanaEumObject','ineum');
 
-          ineum('reportingUrl', 'https://eum-pink-saas.instana.rocks');
+          ineum('reportingUrl', '${NEXT_PUBLIC_REPORTING_URL}');
           ineum('key', '${NEXT_PUBLIC_INSTANA_EUM_KEY}');
           ineum('trackSessions');
       `}
@@ -20,7 +21,7 @@ export const InstanaEumScripts = () => (
       strategy="afterInteractive"
       id="eum"
       crossOrigin="anonymous"
-      src="https://eum.instana.io/eum.min.js"
+      src={`${NEXT_PUBLIC_REPORTING_URL}/eum.min.js`}
       defer
     />
   </>
