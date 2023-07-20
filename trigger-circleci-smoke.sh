fca427858ae697bc1fe06de72010f767d342afbe
lowsky <217931+lowsky@users.noreply.github.com>
2023-07-20 09:50:18 +0200

    hide env keys from showing up in build logs

--- trigger-circleci-smoke.sh
+++ trigger-circleci-smoke.sh
@@ -1,13 +1,16 @@
-#if 0 == echo VERCEL_GITHUB_DEPLOYMENT $VERCEL_GITHUB_DEPLOYMENT
-# exit ...
+#
+# This triggers the e2e smoke tests running on circleci
+# that needs these pipeline parameters:
+# - SUT base url
+# - current commit aka version of given branch
 
-env | sort -u |grep -vi SECRET|grep -vi TOKEN
+env | sort -u |grep -vi SECRET|grep -vi TOKEN |grep -vi key
 
 echo VERCEL_GITHUB_COMMIT_SHA: commit: $VERCEL_GITHUB_COMMIT_SHA
 echo VERCEL_GITHUB_COMMIT_REF: branch: $VERCEL_GITHUB_COMMIT_REF
 echo VERCEL_URL:  $VERCEL_URL
 
-# token: "trigger-from-netlify-hook"
+# TOKEN: "trigger-from-netlify-hook"
 # old: CIRCLE_API_USER_TOKEN=1851154c07ebcb2a01c99a5851a79fe9e8ca893c
 
 #COMMIT
@@ -20,7 +23,7 @@ BRANCH=$VERCEL_GITHUB_COMMIT_REF
 TEST_URL=https://$VERCEL_URL
 
 if [ "${BRANCH}" == "main" ]; then
-  echo testing main page: www.coolboard.fun
+  echo testing main branch deployment: www.coolboard.fun
   TEST_URL="https://www.coolboard.fun"
 fi
 
@@ -32,7 +35,7 @@ CIRCLE="https://circleci.com/api/v2/project"
 URL="$CIRCLE/gh/lowsky/coolboard/pipeline"
 
 echo
-echo triggering url : $URL
+echo triggering circleci build via: $URL
 echo
 
 curl --request POST \
