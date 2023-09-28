575f6b72aa151eb9119fcb27c42828f2409bc9af
lowsky <217931+lowsky@users.noreply.github.com>
2023-09-28 10:14:13 +0200

    refactor(git-commit-check): less global variables

--- smoketest/check-deployed-version/wait-for-deployment.mjs
+++ smoketest/check-deployed-version/wait-for-deployment.mjs
@@ -1,81 +1,69 @@
 import got from 'got';
 
-const url =
-  (process.env.CYPRESS_baseUrl ||
-    'https://coolboard.fun') + '/meta.json';
+const baseUrl = process.env.CYPRESS_baseUrl || 'https://coolboard.fun';
+const url = `${baseUrl}/meta.jsonx`;
 
-const EXPECTED_GIT_SHA1 = process.env.GIT_SHA1 || '';
+const EXPECTED_GIT_SHA1 = process.env.GIT_SHA1;
 
-// fast-fail
 if (!EXPECTED_GIT_SHA1) {
   console.error(
-    'git commit check failed: missing, or empty  ENV variable: GIT_SHA1 (' + EXPECTED_GIT_SHA1 + ') !'
+    `git commit check failed: Please add environment variable: GIT_SHA1`
   );
   process.exit(1);
 }
 
-console.log(
-  'Checking if deployed site on this url: ',
-  url,
-);
+console.log('Checking if deployed site on this url: ', url);
 
-console.log(
-  '...is based on this GIT_SHA1: ',
-  EXPECTED_GIT_SHA1
-);
+console.log('...is based on this GIT_SHA1: ', EXPECTED_GIT_SHA1);
 
-let intTimeMs = 5000; // retry period
-let noRetries = 3; // number retries
+let intTimeMs = 1000; // retry period
+let noRetries = 1; // number retries
 
-let interval;
-let validGitCommitReceived;
+async function checkSiteVersion() {
+  try {
+    const responseAll = await got(url);
+    const response = JSON.parse(responseAll.body);
 
-function checkSiteVersion() {
-  got(url)
-    .then((responseAll) => {
-      const response = JSON.parse(responseAll.body);
+    const validGitCommitReceived = EXPECTED_GIT_SHA1 === response.GIT_SHA1;
 
-      validGitCommitReceived =
-        EXPECTED_GIT_SHA1 === response.GIT_SHA1;
-
-      if (validGitCommitReceived) {
-        console.log(
-          `✓ Deployed site is based on the expected commit. ✓`
-        );
-        if (interval) clearInterval(interval);
-        process.exit(0);
-      }
-      console.warn(
-        `⚠️ Deployed site is based on this commit: ${response.GIT_SHA1} - expected: ${EXPECTED_GIT_SHA1}`
-      );
-    })
-    .catch((err) => {
-      console.info(
-        `Error fetching from ${url} details:`,
-        err
-      );
-    });
+    if (validGitCommitReceived) {
+      console.log(`✓ Deployed site is based on the expected commit. ✓`);
+      return true;
+    }
+    console.warn(
+      `⚠️ Deployed site is based on this commit: ${response.GIT_SHA1} - expected: ${EXPECTED_GIT_SHA1}`
+    );
+  } catch (err) {
+    console.info(`Error fetching from ${url} details:`, err.message, err);
+  }
+  return false;
 }
 
-const retryCheck = () => {
-  checkSiteVersion();
-
-  noRetries--;
-
-  if (noRetries < 0 || validGitCommitReceived) {
-    clearInterval(interval);
-    if (validGitCommitReceived) {
-      process.exit(0);
-    }
-    console.error('Timeout. Error.');
-    process.exit(-1);
+const runCheck = async (noRetries) => {
+  const validGitCommitReceived = await checkSiteVersion();
+  if (validGitCommitReceived) {
+    return true;
   }
+
+  if (noRetries === 0) {
+    return false;
+  }
+
   console.log(
-    ' Will retry check another %d times',
-    noRetries
+    `Will retry check another ${noRetries} times after waiting ${intTimeMs} ms`
   );
+
+  await delay(intTimeMs);
+  await runCheck(noRetries - 1);
 };
 
-checkSiteVersion();
+async function delay(timeMs) {
+  return await new Promise((resolve) => setTimeout(resolve, timeMs));
+}
 
-interval = setInterval(retryCheck, intTimeMs);
+if (await runCheck(noRetries)) {
+  process.exit(0);
+} else {
+  console.error('Timeout. Error.');
+  process.exit(-1);
+}
