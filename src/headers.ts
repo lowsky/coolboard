ee2b74d30dcf070ebbf745d0f1f76a1fb22fc40a
lowsky <217931+lowsky@users.noreply.github.com>
2024-02-24 14:09:50 +0100

    load demo board and sysInfo on client only, enable readonly mode

---
+++ src/headers.ts
@@ -0,0 +1,1 @@
+export const REQ_HEADER_x_coolboard_readonly = 'x-coolboard-readonly';
--- src/setupGraphQLClient.ts
+++ src/setupGraphQLClient.ts
@@ -6,13 +6,20 @@ import {
 } from '@apollo/client';
 import { createNetworkStatusNotifier } from 'react-apollo-network-status';
 import { RetryLink } from '@apollo/client/link/retry';
+import { REQ_HEADER_x_coolboard_readonly } from './headers';
 
 const networkStatusNotifier = createNetworkStatusNotifier();
 export const { useApolloNetworkStatus } = networkStatusNotifier;
 
-export const setupGraphQLClient = () => {
+export const setupGraphQLClient = (readOnly?: boolean) => {
+  const headers = readOnly
+    ? {
+        [REQ_HEADER_x_coolboard_readonly]: 'true',
+      }
+    : {};
   const httpLink = createHttpLink({
     uri: '/api/graphql',
+    headers,
   });
 
   const retryLink = new RetryLink({
