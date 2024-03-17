96479e03c7303f58cd79be47fc2584d6f3a0debd
lowsky <217931+lowsky@users.noreply.github.com>
2024-03-17 20:52:47 +0100

    default

--- server/src/schemaBuilder.ts
+++ server/src/schemaBuilder.ts
@@ -1,8 +1,11 @@
 import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import WithInputPlugin from '@pothos/plugin-with-input';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { DateTimeResolver } from 'graphql-scalars';
+import ws from 'ws';
 
 // Using a type only import will help avoid issues with undeclared
 // exports in esm mode
@@ -11,9 +14,18 @@ import type PrismaTypes from '@pothos/plugin-prisma/generated';
 import { Ctxt } from './resolvers/Context';
 import { isLocalDev } from './helpers/logging';
 
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
 export const prisma = new PrismaClient({
+  errorFormat: 'pretty',
+  adapter,
   log:
-    false && isLocalDev // only for query-debugging:
+    //false &&
+    isLocalDev // only for query-debugging:
       ? ['query', 'info', `warn`, `error`]
       : ['info', 'warn', 'error'],
 });
