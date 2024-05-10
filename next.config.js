150f218582745d0994678aa6c5325bc328b05ced
lowsky <217931+lowsky@users.noreply.github.com>
2024-05-10 14:54:21 +0200

    disable typescript type checking

--- next.config.js
+++ next.config.js
@@ -5,13 +5,18 @@
  **/
 const nextConfig = {
   poweredByHeader: false,
-
   reactStrictMode: true,
-
   eslint: {
     // Warning: This allows production builds to complete even if
     // Your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
 
