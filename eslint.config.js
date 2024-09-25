dd8d2d3f4287dd25a1931f0146357e14b6d75a53
lowsky <217931+lowsky@users.noreply.github.com>
2024-09-25 08:26:53 +0200

    (temporary) remove next-eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,22 +6,20 @@ import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended';
 import { FlatCompat } from '@eslint/eslintrc';
-import { fixupConfigRules } from '@eslint/compat';
-import { includeIgnoreFile } from '@eslint/compat';
+import { fixupConfigRules, includeIgnoreFile } from '@eslint/compat';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
 
-const patchedNextConfig = fixupConfigRules([...compat.extends('next')]);
 const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 const config = [
-  ...patchedNextConfig,
   ...ts.configs.recommended,
   prettierConfigRecommended,
   includeIgnoreFile(gitignorePath),
