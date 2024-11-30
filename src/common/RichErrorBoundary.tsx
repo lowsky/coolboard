3f7e1568588e850a3af0a7a58d58236178e20c7e
lowsky <217931+lowsky@users.noreply.github.com>
2024-11-30 12:13:38 +0100

    formatting of RichErrorBoundary.tsx

--- src/common/RichErrorBoundary.tsx
+++ src/common/RichErrorBoundary.tsx
@@ -3,48 +3,54 @@
  */
 import React, { ReactNode } from 'react';
 
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+} from '@chakra-ui/react';
 
 type State = { error: Error | null };
 
 type Props = {
-    fallback?: ({ error }: State) => ReactNode;
-    message?: string | null;
-    children: React.ReactNode;
+  fallback?: ({ error }: State) => ReactNode;
+  message?: string | null;
+  children: React.ReactNode;
 };
 export default class RichErrorBoundary extends React.Component<Props, State> {
-    state = { error: null };
+  state = { error: null };
 
-    static getDerivedStateFromError(error: Error) {
-        return { error };
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { children, message, fallback } = this.props;
+    const { error } = this.state;
+
+    if (error) {
+      if (message === null) return null;
+
+      if (typeof fallback === 'function') {
+        return fallback({ error });
+      }
+      return (
+        <Alert status="error">
+          <AlertIcon />
+          <Box>
+            <AlertTitle>Error! While trying to load data:</AlertTitle>
+            {message && <AlertDescription>{message}</AlertDescription>}
+            {!message && (
+              <AlertDescription>
+                Details:
+                <pre>{JSON.stringify(error, null, 2)}</pre>
+              </AlertDescription>
+            )}
+          </Box>
+        </Alert>
+      );
     }
-
-    render() {
-        const { children, message, fallback } = this.props;
-        const { error } = this.state;
-
-        if (error) {
-            if (message === null) return null;
-
-            if (typeof fallback === 'function') {
-                return fallback({ error });
-            }
-            return (
-                <Alert status="error">
-                    <AlertIcon />
-                    <Box>
-                        <AlertTitle>Error! While trying to load data:</AlertTitle>
-                        {message && <AlertDescription>{message}</AlertDescription>}
-                        {!message && (
-                            <AlertDescription>
-                                Details:
-                                <pre>{JSON.stringify(error, null, 2)}</pre>
-                            </AlertDescription>
-                        )}
-                    </Box>
-                </Alert>
-            );
-        }
-        return children;
-    }
+    return children;
+  }
 }
