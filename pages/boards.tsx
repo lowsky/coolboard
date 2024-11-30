5778de3672d0861f9d67b46b71c34234d5604dc2
lowsky <217931+lowsky@users.noreply.github.com>
2024-11-30 22:47:45 +0100

    improve auth check

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -4,12 +4,14 @@ import { ProfileHeader } from 'common/ProfileHeader';
 import { FullVerticalContainer } from 'common/FullVerticalContainer';
 import { Boards as BoardList } from 'components/Board/Boards';
 import RichErrorBoundary from 'common/RichErrorBoundary';
+import { useAuth } from 'src/setupInstaWeb';
 
 export default function Boards() {
   // TODO
+  const auth = useAuth();
   const { isSignedIn, isLoaded } = {
-    isLoaded: true,
-    isSignedIn: true,
+    isLoaded: true, // auth.isLoading,
+    isSignedIn: !auth.isLoading && !auth.error && auth.user?.id,
   };
 
   return (
@@ -21,7 +23,10 @@ export default function Boards() {
             {isSignedIn && <BoardList />}
             {!isSignedIn && (
               <Container>
-                <Text>Please, login to see your boards.</Text>
+                <Text>
+                  You will need to be authenticated once, to sync your boards on
+                  this machine and browser.
+                </Text>
               </Container>
             )}
           </>
