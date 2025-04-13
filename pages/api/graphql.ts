e0a0b9f5c909f917c00411059a6d3757467f8f41
lowsky <217931+lowsky@users.noreply.github.com>
2025-04-13 20:53:43 +0200

    migrate to graphql-yoga

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { NextRequest } from 'next/server';
-import { ApolloServer, type ContextFunction } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { ApolloServerPluginUsageReportingDisabled } from '@apollo/server/plugin/disabled';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { createYoga, YogaServer } from 'graphql-yoga';
+import { Plugin } from 'graphql-yoga';
+import { useDisableIntrospection } from '@graphql-yoga/plugin-disable-introspection';
+import { blockFieldSuggestionsPlugin } from '@escape.tech/graphql-armor-block-field-suggestions';
+
 import { getAuth } from '@clerk/nextjs/server';
 import type { PrismaClient } from '@prisma/client';
 
@@ -12,54 +11,57 @@ import type { Ctxt } from 'server/src/resolvers/Context';
 import { buildSchema, prisma } from 'server/src/buildSchema';
 
 import { REQ_HEADER_x_coolboard_readonly } from 'src/headers';
+import { NextRequest } from 'next/server';
 
-const apolloServer = new ApolloServer<Ctxt>({
+type ServerCtxt = object;
+
+const authenticatedHandler: YogaServer<ServerCtxt, Ctxt> = createYoga<
+  ServerCtxt,
+  Ctxt
+>({
   schema: buildSchema(),
-  introspection: Boolean(isLocalDev),
-  allowBatchedHttpRequests: true,
+  batching: true,
+  landingPage: true,
   plugins: [
-    ApolloServerPluginUsageReportingDisabled(),
-    ApolloServerPluginLandingPageLocalDefault({
-      embed: true,
+    useDisableIntrospection({
+      isDisabled: () => !isLocalDev,
     }),
+    blockFieldSuggestionsPlugin(),
+    useAuth(),
   ],
+  context: async ({ request }) => {
+    return {
+      // casting from request to next-Request, required by clerk ...
+      req: request as NextRequest,
+      prisma: prisma as unknown as PrismaClient,
+    };
+  },
+  graphqlEndpoint: '/api/graphql',
 });
 
-const injectContext: ContextFunction<[NextRequest], Ctxt> = async (req) => ({
-  req,
-  prisma: prisma as unknown as PrismaClient,
-});
+function useAuth(): Plugin {
+  return {
+    onRequest({ request, fetchAPI, endResponse }) {
+      const readOnlyHeader = request.headers[REQ_HEADER_x_coolboard_readonly];
+      const isReadOnlyHeader = readOnlyHeader === 'true';
 
-const handleGraphqlRequest = startServerAndCreateNextHandler<NextRequest, Ctxt>(
-  apolloServer,
-  { context: injectContext }
-);
+      if (!isReadOnlyHeader) {
+        const { userId } = getAuth(request as NextRequest);
+        if (!userId) {
+          if (isLocalDev) console.error('    userId is not yet set!');
 
-const authenticatedHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  const readOnlyHeader = req.headers[REQ_HEADER_x_coolboard_readonly];
-  const isReadOnlyHeader = readOnlyHeader === 'true';
-
-  if (isReadOnlyHeader) {
-    await handleGraphqlRequest(req, res);
-    return;
-  }
-
-  try {
-    const { userId } = getAuth(req);
-    if (userId) {
-      await handleGraphqlRequest(req, res);
-      return;
-    }
-  } catch (e) {
-    console.error(e);
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  isLocalDev && console.error('    userId is not yet set!');
-
-  res.status(401).json({ id: null });
-};
+          endResponse(
+            new fetchAPI.Response(null, {
+              status: 401,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            })
+          );
+        }
+      }
+    },
+  };
+}
 
 export default authenticatedHandler;
