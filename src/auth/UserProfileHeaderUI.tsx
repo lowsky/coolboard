c0da9f0ceed60c4805d45518a2bb118d8ba44dd7
lowsky <217931+lowsky@users.noreply.github.com>
2025-08-03 19:50:49 +0200

    add dark mode switch

--- src/auth/UserProfileHeaderUI.tsx
+++ src/auth/UserProfileHeaderUI.tsx
@@ -3,16 +3,18 @@ import { useApolloClient } from '@apollo/client';
 import { SignedIn, SignedOut, useClerk, UserButton } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import { useRouter } from 'next/router';
-import { Button, Flex, useColorMode } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { FaSignOutAlt } from 'react-icons/fa';
 
 import { LoginButton } from 'auth/LoginButton';
+import { useTheme } from 'next-themes';
+import { ColorModeButton } from 'components/ui/color-mode';
 
 export const UserProfileHeaderUI = () => {
   const apolloClient = useApolloClient();
   const { signOut } = useClerk();
   const { replace } = useRouter();
-  const { colorMode } = useColorMode();
+  const colorMode = useTheme().resolvedTheme;
 
   const clerkAppearance = colorMode === 'dark' ? { baseTheme: dark } : {};
 
@@ -26,27 +28,30 @@ export const UserProfileHeaderUI = () => {
   return (
     <>
       <>
-        <SignedOut>
-          <LoginButton />
-        </SignedOut>
+        {
+          // @ts-expect-error Its return type 'Promise<any>' is not a valid JSX element.
+          <SignedOut>
+            <LoginButton />
+            <ColorModeButton />
+          </SignedOut>
+        }
       </>
-      <SignedIn>
-        <Flex alignItems="center" gap="0.5em">
-          <UserButton appearance={clerkAppearance} afterSignOutUrl="/" />
-          <Button
-            // @ts-expect-error it is an async function.
-            onClick={awaitLogoutAndCacheClear}
-            data-cy="sign-out-button"
-            leftIcon={
-              <div>
-                <FaSignOutAlt />
-              </div>
-            }
-            color="unset">
-            Sign Out
-          </Button>
-        </Flex>
-      </SignedIn>
+      {
+        // @ts-expect-error Its return type 'Promise<any>' is not a valid JSX element.
+        <SignedIn>
+          <Flex alignItems="center" gap="0.5em">
+            <ColorModeButton />
+            <UserButton appearance={clerkAppearance} afterSignOutUrl="/" />
+            <Button
+              // @ts-expect-error it is an async function.
+              onClick={awaitLogoutAndCacheClear}
+              data-cy="sign-out-button">
+              <FaSignOutAlt />
+              Sign Out
+            </Button>
+          </Flex>
+        </SignedIn>
+      }
     </>
   );
 };
