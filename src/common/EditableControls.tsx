17288223d5b5ac59d2e54a7337ea7b327de78a93
lowsky <217931+lowsky@users.noreply.github.com>
2025-08-03 20:32:01 +0200

    migrate EditableControl

--- src/common/EditableControls.tsx
+++ src/common/EditableControls.tsx
@@ -1,19 +1,20 @@
-import { Button, ButtonGroup, useEditableControls } from '@chakra-ui/react';
+import { Button, Flex, Editable } from '@chakra-ui/react';
 import React from 'react';
 
 export function EditableControls() {
-  const { isEditing, getSubmitButtonProps, getCancelButtonProps } =
-    useEditableControls();
-  return isEditing ? (
-    <ButtonGroup variant="outline" spacing="6">
-      <Button
-        background="green"
-        color="white"
-        _hover={{ background: 'darkgreen' }}
-        {...getSubmitButtonProps()}>
-        Create
-      </Button>
-      <Button {...getCancelButtonProps()}>Cancel</Button>
-    </ButtonGroup>
-  ) : null;
+  return (
+    <Flex gap="6">
+      <Editable.SubmitTrigger asChild>
+        <Button
+          background="green"
+          color="white"
+          _hover={{ background: 'darkgreen' }}>
+          Create
+        </Button>
+      </Editable.SubmitTrigger>
+      <Editable.CancelTrigger asChild>
+        <Button variant="outline">Cancel</Button>
+      </Editable.CancelTrigger>
+    </Flex>
+  );
 }
