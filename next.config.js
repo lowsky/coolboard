880f3a6a318fa62437265da7d3b4db0bde69a056
lowsky <217931+lowsky@users.noreply.github.com>
2025-11-21 13:53:30 +0100

    remove obsolete debug message

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// @ts-check
-
-// eslint-disable-next-line no-undef
-const disableOtel = process.env.OTEL_DISABLED;
-const instrumentationHook = disableOtel !== 'true';
-// eslint-disable-next-line no-undef
-console.log(
-  `instrumentation via OTEL: ${
-    instrumentationHook ? 'enabled' : 'disabled'
-  } - by env: OTEL_DISABLED=${disableOtel}`
-);
-
 /**
  * @type {import('next').NextConfig}
  **/
