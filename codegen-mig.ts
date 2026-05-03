dbf8c8479cc7027cbb767d4026506dd649107f71
lowsky <217931+lowsky@users.noreply.github.com>
2026-05-03 23:04:23 +0200

    adopt codegen script

--- codegen-mig.ts
+++ codegen-mig.ts
@@ -1,13 +1,14 @@
-import type { Types } from '@graphql-codegen/plugin-helpers';
 import { defineConfig } from '@eddeee888/gcg-operation-location-migration';
 
 import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { Types } from '@graphql-codegen/plugin-helpers';
 import { lexicographicSortSchema, printSchema } from 'graphql';
 
 import { buildSchema } from './server/src/buildSchema';
 
 const clientSide: Types.ConfiguredOutput | Types.ConfiguredPlugin[] = {
-  schema: printSchema(lexicographicSortSchema(buildSchema())),
+  //schema: printSchema(lexicographicSortSchema(buildSchema())),
+  schema: 'server/src/schema/schema.graphql',
   documents: ['src/common/**/*.graphql', 'src/components/**/*.graphql'],
   preset: 'client',
   plugins: [
@@ -37,10 +38,9 @@ const clientSide: Types.ConfiguredOutput | Types.ConfiguredPlugin[] = {
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: printSchema(lexicographicSortSchema(buildSchema())),
-  //schema: 'src/**/*.graphqls', // 👈 This points to your usual GraphQL schema endpoint or files.
-  documents: ['src/common/**/*.graphql', 'src/components/**/*.graphql'],
-  //documents: 'src/**/*.graphql', // 👈 This points to your operation files.
+  //schema: printSchema(lexicographicSortSchema(buildSchema())),
+  schema: 'server/src/schema/schema.graphql', // 👈 This points to your usual GraphQL schema endpoint or files.
+  documents: 'src/**/*.graphql', // 👈 This points to your operation files.
   generates: {
     /*
      * 💡 Example of index file generation approach
@@ -54,45 +54,37 @@ const config: CodegenConfig = {
      * 💡 Example of near-operation file approach
      * Once migrated, you can remove this completely
      */
-    // 'src/graphql/types.ts': {
-    //   plugins: ['typescript'],
-    // },
-    // './src/': {
-    //   preset: 'near-operation-file',
-    //   presetConfig: {
-    //     baseTypesPath: './graphql/types.ts',
-    //     extension: '.generated.ts',
-    //   },
-    //   plugins: ['typescript-operations', 'typescript-react-apollo'],
-    // },
+    /*
+    'src/generated/graphql.ts': {
+      plugins: ['typescript'],
+    },
+    './src/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        baseTypesPath: './graphql/types.ts',
+        extension: '.generated.ts',
+      },
+      plugins: ['typescript-operations', 'typescript-react-apollo'],
+    },
+
+     */
 
     // 💡 Codemod config example
     // `src/` is the base path where we want to run the codemod.
     'src/': defineConfig({
+      //  targetStyle: 'near-operation-file',
       tsConfigFilePath: './tsconfig.json', // 👈 Path from project root to your project tsconfig.json (Note: not from the base path).
       gqlTag: {
-        name: 'graphql', // 👈 The tag used to parse operation documents.
+        name: 'gql', // 👈 The tag used to parse operation documents.
         importFrom: './gql', // 👈 The the module to import the graphql tag.
         importType: 'relative', // 👈 Whether `importFrom` is relative or absolute. If relative, the path from the base path to the module.
       },
       hooksImportFrom: '@apollo/client/react', // 👈 The module to import Apollo Client hooks. Use @apollo/client for older Apollo Client v3 versions.
     }),
   },
-};
-
-const config2: CodegenConfig = {
-  overwrite: true,
-  //schema: 'server/src/schema/schema.graphql',
-  generates: {
-    'server/src/schema/schema.graphql': {
-      schema: printSchema(lexicographicSortSchema(buildSchema())),
-      plugins: ['schema-ast'],
-      watchPattern: 'server/src/buildSchema.*',
-    },
-    'src/generated/': clientSide,
-  },
   hooks: {
     afterAllFileWrite: ['prettier --write'],
   },
 };
+
 export default config;
--- codegen.ts
+++ codegen.ts
@@ -2,15 +2,49 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 import { lexicographicSortSchema, printSchema } from 'graphql';
 
 import { buildSchema } from './server/src/buildSchema';
+import type { Types } from '@graphql-codegen/plugin-helpers';
+
+const clientSide: Types.ConfiguredOutput | Types.ConfiguredPlugin[] = {
+  //schema: printSchema(buildSchema()),
+  //schema: 'server/src/schema/schema.graphql',
+  documents: ['src/**/*.tsx'],
+  preset: 'client',
+  //plugins: [
+  //
+  //'typescript',
+  //'typescript-operations',
+  //'typescript-react-apollo',
+  //],
+  presetConfig: {
+    // codegen's masking is incompatible with apollo with preset-client:
+    // https://www.apollographql.com/docs/react/data/fragments#with-the-client-preset
+    // disables the incompatible GraphQL Codegen fragment masking feature:
+    fragmentMasking: false,
+  },
+  config: {
+    reactApolloVersion: 4,
+
+    // need to add when fragmentMasking is disabled:
+    // https://www.apollographql.com/docs/react/data/fragments#with-the-client-preset
+    customDirectives: {
+      apolloUnmask: true,
+    },
+    inlineFragmentTypes: 'mask',
+  },
+};
 
 const config: CodegenConfig = {
-  overwrite: true,
+  schema: 'server/src/schema/schema.graphql',
+  ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
+    /* LATER
     'server/src/schema/schema.graphql': {
       schema: printSchema(lexicographicSortSchema(buildSchema())),
       plugins: ['schema-ast'],
       watchPattern: 'server/src/buildSchema.*',
     },
+     */
+    'src/gql/': clientSide,
   },
   hooks: {
     afterAllFileWrite: ['prettier --write'],
