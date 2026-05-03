a6e1c59b8836bd9abbc04782f820f67417a996cf
lowsky <217931+lowsky@users.noreply.github.com>
2026-05-03 17:08:34 +0200

    avoid graphql-codegen issue and generate server/src/schema/schema.graphql

--- codegen.ts
+++ codegen.ts
@@ -1,23 +1,16 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
-import { printSchema } from 'graphql';
+import { lexicographicSortSchema, printSchema } from 'graphql';
 
 import { buildSchema } from './server/src/buildSchema';
 
-const clientSide = {
-  schema: printSchema(buildSchema()),
-  //schema: 'server/src/schema/schema.graphql',
-  documents: ['src/**/*.graphql'],
-  plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
-};
 const config: CodegenConfig = {
   overwrite: true,
   generates: {
     'server/src/schema/schema.graphql': {
-      schema: printSchema(buildSchema()),
+      schema: printSchema(lexicographicSortSchema(buildSchema())),
       plugins: ['schema-ast'],
       watchPattern: 'server/src/buildSchema.*',
     },
-    'src/generated/graphql.tsx': clientSide,
   },
   hooks: {
     afterAllFileWrite: ['prettier --write'],
