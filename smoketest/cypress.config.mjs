cebcebb0568d339ed3a9f345f747b700a5b0a68f
lowsky <217931+lowsky@users.noreply.github.com>
2026-08-02 15:05:49 +0200

    enable recording in cypress cloud

--- smoketest/cypress.config.mjs
+++ smoketest/cypress.config.mjs
@@ -4,22 +4,20 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   experimentalWebKitSupport: true,
-  projectId: '8p1ybc',
+  projectId: 'epr8fo',
   defaultCommandTimeout: 8000,
   // https://docs.cypress.io/app/references/experiments#Experimental-Fast-Visibility
   experimentalFastVisibility: true,
   expose: {
-    "LOGIN": "skylab@nurfuerspam.de",
-    "PRODUCTION_LOGIN": "coolboard+testnew@protonmail.com",
+    LOGIN: 'skylab@nurfuerspam.de',
+    PRODUCTION_LOGIN: 'coolboard+testnew@protonmail.com',
   },
   e2e: {
     setupNodeEvents(on, config) {
       return clerkSetup({ config });
     },
 
-    blockHosts: [
-      'eum.instana.com'
-    ],
+    blockHosts: ['eum.instana.com'],
     baseUrl: 'https://www.coolboard.eu',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
