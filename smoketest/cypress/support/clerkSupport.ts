667b86ae0f8ebcf4aefa006d0980c49a1e97e8c2
lowsky <217931+lowsky@users.noreply.github.com>
2026-09-16 09:57:41 +0200

    workaround for @clerk/testing not compatible with cypress 16,

---
+++ smoketest/cypress/support/clerkSupport.ts
@@ -0,0 +1,262 @@
+// adopted clerk testing code
+// it was not compatible with cypress 16, while still using old Cypress.env
+//
+// from https://github.com/clerk/javascript/blob/71c3d0703e60870a1eb6074d1fbf4540c1bf905c/packages/testing/src/cypress/index.ts
+
+// still hard-coded and needs adoption:
+//    frontendApiUrl
+//
+// ---
+
+import type { Clerk } from '@clerk/shared/types';
+
+//--- constants
+export const TESTING_TOKEN_PARAM = "__clerk_testing_token";
+
+export const ERROR_MISSING_FRONTEND_API_URL =
+  'The Clerk Frontend API URL is required to bypass bot protection. ' +
+  'Make sure the clerkSetup function is called during your global setup before setupClerkTestingToken is called.';
+
+declare global {
+  interface Window {
+    Clerk: Clerk;
+  }
+}
+
+type AddClerkCommandsParams = {
+  Cypress: typeof Cypress;
+  cy: Cypress.Chainable;
+};
+export type SetupClerkTestingTokenOptions = {
+  frontendApiUrl?: string;
+};
+type SetupClerkTestingTokenParams = {
+  options?: SetupClerkTestingTokenOptions;
+};
+
+
+const setupClerkTestingToken = (params?: SetupClerkTestingTokenParams) => {
+  const fapiUrl = params?.options?.frontendApiUrl
+  if (!fapiUrl) {
+    throw new Error(ERROR_MISSING_FRONTEND_API_URL);
+  }
+  const apiUrl = `https://${fapiUrl}/v1/**`;
+
+  cy.env(
+    ['CLERK_TESTING_TOKEN']).then(({ CLERK_TESTING_TOKEN }) => {
+    const testingToken = CLERK_TESTING_TOKEN;
+    cy.log('testing token:', testingToken);
+    expect(testingToken).to.be.a('string');
+
+    cy.intercept(apiUrl, req => {
+      if (testingToken) {
+        req.query[TESTING_TOKEN_PARAM] = testingToken;
+      }
+
+      req.continue();
+
+      req.on('response', res => {
+        // Override captcha_bypass in /v1/client
+        if (res.body?.response?.captcha_bypass === false) {
+          res.body.response.captcha_bypass = true;
+        }
+
+        // Override captcha_bypass in piggybacking
+        if (res.body?.client?.captcha_bypass === false) {
+          res.body.client.captcha_bypass = true;
+        }
+      });
+    });
+  });
+};
+
+
+export const addClerkCommands = ({ Cypress, cy }: AddClerkCommandsParams) => {
+  Cypress.Commands.add(`clerkSignIn`, signInParams => {
+    let params:SetupClerkTestingTokenParams = {options: {
+        frontendApiUrl: 'clerk.coolboard.eu'// or Cypress.env('CLERK_FAPI');
+      }
+    };
+    setupClerkTestingToken(params);
+
+    cy.log(`Clerk: Signing in...`);
+
+    cy.window()
+      .should(window => {
+        expect(window).to.not.have.property(`Clerk`, undefined);
+        expect(window.Clerk.loaded).to.eq(true);
+      })
+      .then(async window => {
+        await signInHelper({ windowObject: window, signInParams });
+        cy.log(`Clerk: Finished signing in.`);
+      });
+  });
+
+  Cypress.Commands.add(`clerkSignOut`, signOutOptions => {
+    cy.log(`Clerk: Signing out...`);
+
+    cy.window()
+      .should(window => {
+        expect(window).to.not.have.property(`Clerk`, undefined);
+        expect(window.Clerk.loaded).to.eq(true);
+      })
+      .then(async window => {
+        await window.Clerk.signOut(signOutOptions);
+        cy.log(`Clerk: Finished signing out.`);
+      });
+  });
+
+  Cypress.Commands.add(`clerkLoaded`, () => {
+    cy.window().should(window => {
+      expect(window).to.not.have.property(`Clerk`, undefined);
+      expect(window.Clerk.loaded).to.eq(true);
+    });
+  });
+};
+
+
+
+import type { EmailCodeFactor, PhoneCodeFactor, SignInFirstFactor } from '@clerk/shared/types';
+
+
+export type ClerkSignInParams =
+  | {
+  strategy: 'password';
+  password: string;
+  identifier: string;
+}
+  | {
+  strategy: 'phone_code' | 'email_code';
+  identifier: string;
+}
+  | {
+  strategy: 'ticket';
+  ticket: string;
+};
+
+export type SignInHelperParams = {
+  signInParams: ClerkSignInParams;
+  windowObject?: Window;
+};
+//import type { SignInHelperParams } from './types';
+
+// This function is serialized and executed in the browser context
+export const signInHelper = async ({ signInParams, windowObject }: SignInHelperParams) => {
+  try {
+    const w = windowObject || window;
+    if (!w.Clerk.client) {
+      return;
+    }
+
+    const signIn = w.Clerk.client.signIn;
+
+    switch (signInParams.strategy) {
+      case 'password': {
+        const res = await signIn.create(signInParams);
+        await w.Clerk.setActive({
+          session: res.createdSessionId,
+        });
+        break;
+      }
+
+      case 'ticket': {
+        const res = await signIn.create({
+          strategy: 'ticket',
+          ticket: signInParams.ticket,
+        });
+
+        if (res.status === 'complete') {
+          await w.Clerk.setActive({
+            session: res.createdSessionId,
+          });
+        } else {
+          throw new Error(`Sign-in with ticket failed. Status: ${res.status}`);
+        }
+        break;
+      }
+
+      case 'phone_code': {
+        // Assert that the identifier is a test phone number
+        if (!/^\+1\d{3}55501\d{2}$/.test(signInParams.identifier)) {
+          throw new Error(
+            `Phone number should be a test phone number.\n
+       Example: +1XXX55501XX.\n
+       Learn more here: https://clerk.com/docs/testing/test-emails-and-phones#phone-numbers`,
+          );
+        }
+
+        // Sign in with phone code
+        const { supportedFirstFactors } = await signIn.create({
+          identifier: signInParams.identifier,
+        });
+        const phoneFactor = supportedFirstFactors?.find(
+          (factor: SignInFirstFactor): factor is PhoneCodeFactor => factor.strategy === 'phone_code',
+        );
+
+        if (phoneFactor) {
+          await signIn.prepareFirstFactor({
+            strategy: 'phone_code',
+            phoneNumberId: phoneFactor.phoneNumberId,
+          });
+          const signInAttempt = await signIn.attemptFirstFactor({
+            strategy: 'phone_code',
+            code: '424242',
+          });
+
+          if (signInAttempt.status === 'complete') {
+            await w.Clerk.setActive({ session: signInAttempt.createdSessionId });
+          } else {
+            throw new Error(`Status is ${signInAttempt.status}`);
+          }
+        } else {
+          throw new Error('phone_code is not enabled.');
+        }
+        break;
+      }
+
+      case 'email_code': {
+        // Assert that the identifier is a test email
+        if (!signInParams.identifier.includes('+clerk_test')) {
+          throw new Error(
+            `Email should be a test email.\n
+       Any email with the +clerk_test subaddress is a test email address.\n
+       Learn more here: https://clerk.com/docs/testing/test-emails-and-phones#email-addresses`,
+          );
+        }
+
+        // Sign in with email code
+        const { supportedFirstFactors } = await signIn.create({
+          identifier: signInParams.identifier,
+        });
+        const emailFactor = supportedFirstFactors?.find(
+          (factor: SignInFirstFactor): factor is EmailCodeFactor => factor.strategy === 'email_code',
+        );
+
+        if (emailFactor) {
+          await signIn.prepareFirstFactor({
+            strategy: 'email_code',
+            emailAddressId: emailFactor.emailAddressId,
+          });
+          const signInAttempt = await signIn.attemptFirstFactor({
+            strategy: 'email_code',
+            code: '424242',
+          });
+
+          if (signInAttempt.status === 'complete') {
+            await w.Clerk.setActive({ session: signInAttempt.createdSessionId });
+          } else {
+            throw new Error(`Status is ${signInAttempt.status}`);
+          }
+        } else {
+          throw new Error('email_code is not enabled.');
+        }
+        break;
+      }
+
+      default:
+        throw new Error(`Unsupported strategy: ${(signInParams as any).strategy}`);
+    }
+  } catch (err: any) {
+    throw new Error(`Clerk: Failed to sign in: ${err?.message}`);
+  }
+};
